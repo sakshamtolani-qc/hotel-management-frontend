5d8d7f0e863d2bf63b7f3c4fb584df7f00e12096
Aniket-Todkar23 <154717844+Aniket-Todkar23@users.noreply.github.com>
2025-09-06 13:41:18 +0530

    HMCRM-FEHC-1 Added login page frontend integrated jwt just remained t… (#2)
    * HMCRM-FEHC-1 Added login page frontend integrated jwt just remained to be linked
    
    * HMCRM-FEHC-1 added JWT Auth with failing msg
    
    * HMCRM-FEHC-1 Added signUp page
    
    * HMCRM-FEHC-1 Added signUp page
    
    * Fixed Bedroom Image in Login Page. Also improved folder structure
    
    ---------
    
    Co-authored-by: SakshamTolani <hr@quoriumconsulting.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export async function apiFetch(
+  url: string,
+  options: RequestInit = {}
+): Promise<Response> {
+  const token = localStorage.getItem("token");
+
+  return fetch(`${import.meta.env.VITE_API_URL}${url}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...options.headers,
+    },
+  });
 }
