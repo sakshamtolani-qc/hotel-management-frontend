61217d05b1aa6db76b34fdbed5e122f64b9ce6e2
SakshamTolani <hr@quoriumconsulting.com>
2025-09-06 17:18:42 +0530

    Fixed Main Branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-=======
 import { Toaster } from "@/utils/toaster";
 import { Toaster as Sonner } from "@/utils/sonner";
 import { TooltipProvider } from "@/utils/tooltip";
@@ -57,7 +30,5 @@ const App = () => (
     </TooltipProvider>
   </QueryClientProvider>
 );
-
->>>>>>> e92c883a (Implement Room List View)
 
 export default App;
